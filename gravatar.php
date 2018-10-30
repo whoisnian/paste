8b582bba36cce5934356bb0a8bed6216c34c15b1
whoisnian <zhuchangbao1998@gmail.com>
2018-10-30 23:18:20 +0800

    sed ori header

--- gravatar.php
+++ gravatar.php
@@ -1,5 +1,13 @@
 <?php
-$url = "http://www.gravatar.com/avatar/".md5($_GET["user"])."?s=".$_GET["s"]."&d=".$_GET["d"];
-header("Content-Type: image/jpeg");
-echo file_get_contents($url);
+if(isset($_GET["user"])&&isset($_GET["s"])&&isset($_GET["d"])) {
+    $url = "http://www.gravatar.com/avatar/".md5($_GET["user"])."?s=".$_GET["s"]."&d=".$_GET["d"];
+}
+else {
+    $url = "http://www.gravatar.com/avatar/1f2bf2e032a11fec0ea5d7f9d6a5aa1d?s=48&d=retro";
+}
+$content = file_get_contents($url);
+foreach($http_response_header as $i) {
+    header($i);
+}
+echo $content;
 ?>
